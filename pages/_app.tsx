cbcc096d8c94c6cd247056c64f0afcadd70a64fb
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-12 00:30:36 +0900

    add--layout 適応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Layout } from "../components/Layout";
+import { darkTheme, brightTheme } from "../theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={darkTheme} resetCSS>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
