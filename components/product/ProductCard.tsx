c50f0639e570d559d7920e1ca62aa7fda18c361f
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-12 20:03:09 +0900

    update--product external link.

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,5 +1,7 @@
-import { Flex, Image, Stack, Text, Box } from "@chakra-ui/react";
+import { Flex, Image, Stack, Text, Box, Link } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 import { ProductType } from "../../types/product";
 
 type Props = {
@@ -12,23 +14,37 @@ const imageOption = "?fm=webp&w=500";
 export const ProductCard: VFC<Props> = memo((props) => {
   const { product } = props;
   return (
-    <Box>
-      <Flex bg="gray.700" borderTopRadius={10}>
+    <Box
+      position="relative"
+      border="1px"
+      borderColor="gray.300"
+      borderRadius={10}
+      overflow="hidden"
+    >
+      {product.url && (
+        <Link
+          href={product.url}
+          isExternal
+          position="absolute"
+          top={2}
+          right={2}
+          bg="gray.300"
+          p={1}
+          borderRadius={4}
+          color="gray.900"
+        >
+          <FaExternalLinkAlt fontSize="20px" />
+        </Link>
+      )}
+      <Flex>
         <Image
           w="100%"
           objectFit="cover"
-          borderTopRadius={10}
           src={`${product["main-image"].url}${imageOption}`}
           alt={product.title}
         />
       </Flex>
-      <Stack
-        p={4}
-        bg="gray.300"
-        color="gray.900"
-        spacing={1}
-        borderBottomRadius={10}
-      >
+      <Stack p={4} bg="gray.300" color="gray.900" spacing={1}>
         <Text fontSize="lg">{product.title}</Text>
         <Text fontSize="md">{product.sumally}</Text>
         <Text fontSize="sm" color="gray.600">
