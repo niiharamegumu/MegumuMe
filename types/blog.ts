f6e74fa9933adebeb56a7dd6ee19b8e70d80ea8a
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-12 22:56:13 +0900

    update--type

--- types/blog.ts
+++ types/blog.ts
@@ -1,16 +1,15 @@
-export type ProductType = {
+export type BlogType = {
   id: string;
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
   revisedAt: string;
   title: string;
+  body: string;
+  description: string;
   "main-image": {
     url: string;
     height: number;
     width: number;
   };
-  sumally: string;
-  skills: Array<string>;
-  url?: string;
 };
