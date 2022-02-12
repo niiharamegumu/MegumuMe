14edd8dfd24696c562277d4a705a8464d296db3c
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-12 22:56:50 +0900

    update--cms client.

--- libs/microCMS/client.ts
+++ libs/microCMS/client.ts
@@ -1,18 +1,30 @@
 import { createClient } from "microcms-js-sdk";
 
 const productsApiKey = process.env.NEXT_PUBLIC_PRODUCTS_API_KEY;
+const blogsApiKey = process.env.NEXT_PUBLIC_BLOGS_API_KEY;
 const productsServiceDomain = process.env.NEXT_PUBLIC_PRODUCTS_SERVICE_DOMAIN;
+const blogsServiceDomain = process.env.NEXT_PUBLIC_BLOGS_SERVICE_DOMAIN;
 
 if (productsApiKey === undefined)
   throw Error(
     ".envファイルに`NEXT_PUBLIC_PRODUCTS_API_KEY`を設定してください。"
   );
+if (blogsApiKey === undefined)
+  throw Error(".envファイルに`NEXT_PUBLIC_BLOGS_API_KEY`を設定してください。");
 if (productsServiceDomain === undefined)
   throw Error(
     ".envファイルに`NEXT_PUBLIC_PRODUCTS_SERVICE_DOMAIN`を設定してください。"
+  );
+if (blogsServiceDomain === undefined)
+  throw Error(
+    ".envファイルに`NEXT_PUBLIC_BLOGS_SERVICE_DOMAIN`を設定してください。"
   );
 
 export const clientProducts = createClient({
   serviceDomain: productsServiceDomain,
   apiKey: productsApiKey,
+});
+export const clientBlogs = createClient({
+  serviceDomain: blogsServiceDomain,
+  apiKey: blogsApiKey,
 });
