c3badc9b44a240ae2177d4bbff4dd19850d10ed7
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-13 13:05:22 +0900

    update--to blog detail link.

--- components/blogs/BlogCard.tsx
+++ components/blogs/BlogCard.tsx
@@ -1,5 +1,5 @@
 import { memo, VFC } from "react";
-import { Box, Flex, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
 import { format } from "date-fns";
 
 import { BlogType } from "../../types/blog";
@@ -12,22 +12,29 @@ const imageOption = "?fm=webp&w=500";
 export const BlogCard: VFC<Props> = memo((props) => {
   const { blog } = props;
   return (
-    <Flex borderRadius={10} overflow="hidden" bg="gray.300" alignItems="center">
-      <Image
-        src={`${blog.mainImage.url}${imageOption}`}
-        alt={blog.title}
-        objectFit="cover"
-        w={{ base: "42vw", lg: "25vw" }}
-        h={{ base: "140px", sm: "220px" }}
-      />
-      <Box color="gray.900" p={{ base: 2, md: 4 }}>
-        <Text fontSize={{ base: "sm", sm: "lg", md: "xl" }} fontWeight="bold">
-          {blog.title}
-        </Text>
-        <Text fontSize={{ base: "xs", sm: "md" }} color="gray.600">
-          {format(new Date(blog.updatedAt), "yyyy.MM.dd")}
-        </Text>
-      </Box>
-    </Flex>
+    <Link href={`/blogs/${blog.id}`} _hover={{ border: "none" }}>
+      <Flex
+        borderRadius={10}
+        overflow="hidden"
+        bg="gray.300"
+        alignItems="center"
+      >
+        <Image
+          src={`${blog.mainImage.url}${imageOption}`}
+          alt={blog.title}
+          objectFit="cover"
+          w={{ base: "42vw", lg: "25vw" }}
+          h={{ base: "140px", sm: "220px" }}
+        />
+        <Box color="gray.900" p={{ base: 2, md: 4 }}>
+          <Text fontSize={{ base: "sm", sm: "lg", md: "xl" }} fontWeight="bold">
+            {blog.title}
+          </Text>
+          <Text fontSize={{ base: "xs", sm: "md" }} color="gray.600">
+            {format(new Date(blog.updatedAt), "yyyy.MM.dd")}
+          </Text>
+        </Box>
+      </Flex>
+    </Link>
   );
 });
