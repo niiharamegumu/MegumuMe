24cc4522d1e2215de6397d2acaa5ebae17de584b
Megumu Niihara <nhrmgm1223@gmail.com>
2022-03-29 17:38:12 +0900

    update: blog cart.

--- components/blogs/BlogCard.tsx
+++ components/blogs/BlogCard.tsx
@@ -5,25 +5,44 @@ import { format } from "date-fns";
 import { BlogType } from "../../types/blog";
 import { TagButton } from "../TagButton";
 
-type Props = { blog: BlogType };
+type Props = {
+  link: string;
+  title: string;
+  createdAt: string;
+  isTargetBlank?: boolean;
+  tags?: BlogType["tags"];
+};
 
 // eslint-disable-next-line react/display-name
 export const BlogCard: VFC<Props> = memo((props) => {
-  const { blog } = props;
+  const { link, title, createdAt, isTargetBlank, tags } = props;
+  const linkProps = (url: string) => {
+    if (url.match("^http")) {
+      return {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      };
+    }
+    return {};
+  };
   return (
-    <Link href={`/blogs/${blog.id}`} _hover={{ border: "none" }}>
+    <Link
+      href={link}
+      _hover={{ border: "none" }}
+      {...(isTargetBlank ? linkProps(link) : {})}
+    >
       <Box color="gray.900" p={{ base: 4, md: 6 }}>
         <Text
           fontSize={{ base: "md", sm: "lg", md: "xl" }}
           fontWeight="bold"
           mb={2}
         >
-          {blog.title}
+          {title}
         </Text>
-        {blog.tags &&
-          blog.tags.map((tag) => <TagButton key={tag.id} tag={tag.tagName} />)}
+        {tags &&
+          tags.map((tag) => <TagButton key={tag.id} tag={tag.tagName} />)}
         <Text fontSize={{ base: "xs", sm: "md" }} color="gray.600" mt={2}>
-          {format(new Date(blog.createdAt), "yyyy.MM.dd")}
+          {format(new Date(createdAt), "yyyy.MM.dd")}
         </Text>
       </Box>
     </Link>
