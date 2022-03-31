cd4b6ff2b0bf99842c3840ba44b369751852048c
Megumu Niihara <nhrmgm1223@gmail.com>
2022-03-31 10:19:36 +0900

    update: memo化

--- components/style/Common.tsx
+++ components/style/Common.tsx
@@ -1,7 +1,8 @@
-import { VFC } from "react";
+import { memo, VFC } from "react";
 import { Heading, HeadingProps } from "@chakra-ui/react";
 
-export const HeadH2: VFC<HeadingProps> = (props) => {
+// eslint-disable-next-line react/display-name
+export const HeadH2: VFC<HeadingProps> = memo((props) => {
   const { children } = props;
   return (
     <Heading
@@ -14,12 +15,14 @@ export const HeadH2: VFC<HeadingProps> = (props) => {
       {children}
     </Heading>
   );
-};
-export const HeadH3: VFC<HeadingProps> = (props) => {
+});
+
+// eslint-disable-next-line react/display-name
+export const HeadH3: VFC<HeadingProps> = memo((props) => {
   const { children } = props;
   return (
     <Heading as="h3" fontSize="xl" pb={{ base: 2, md: 4 }} {...props}>
       {children}
     </Heading>
   );
-};
+});
