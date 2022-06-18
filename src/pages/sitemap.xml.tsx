efd4dea589ade0d2a6ac4e841a7a911f8ae781b0
Niihara Megumu <nhrmgm1223@gmail.com>
2022-06-18 21:50:06 +0900

    Revert "remove"
    This reverts commit 1508be261933ede724e025f545c3dffa90e651cb.

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,36 @@
+import { format } from 'date-fns'
+import { GetServerSideProps } from 'next'
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap'
+
+import { clientBlogs } from '../libs/microCMS/client'
+
+type PostData = {
+  id: string
+  updatedAt: string
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const data = await clientBlogs.get({
+    endpoint: 'blogs',
+    queries: { fields: 'id,updatedAt' }
+  })
+  const postsData = data.contents
+
+  const fields: ISitemapField[] = []
+  postsData.forEach((data: PostData) => {
+    fields.push({
+      loc: `${process.env.NEXT_PUBLIC_SITE_URL}/article/${data.id}`,
+      lastmod: format(new Date(data.updatedAt), 'yyyy-MM-dd'),
+      priority: 1,
+      changefreq: 'daily'
+    })
+  })
+  context.res.setHeader(
+    'Cache-Control',
+    'max-age=86400, stale-while-revalidate=360'
+  )
+  return getServerSideSitemap(context, fields)
+}
+
+const Sitemap = () => null
+export default Sitemap
