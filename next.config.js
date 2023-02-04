30fabc88fae2eb68c8a6f51a61fae95c1adbe3a5
Niihara Megumu <nhrmgm1223@gmail.com>
2023-02-04 17:33:54 +0900

    next-pwaの排除

--- next.config.js
+++ next.config.js
@@ -5,18 +5,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-/** @type {import('next/dist/next-server/server/config-shared').NextConfig} */
-
-const withPWA = require('next-pwa')
-const runtimeCaching = require('next-pwa/cache')
-
-const config = {
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/]
-  }
-}
-
-module.exports = withPWA(config)
